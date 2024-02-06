eb6e1cc16696b8961ddae8f711802f54b3955840
Henrysoed <henry.soedibjo@gmail.com>
2024-02-06 22:01:44 +0700

    Fix mid error in Edit and Delete feature

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -1,5 +1,4 @@
 package id.ac.ui.cs.advprog.eshop.repository;
-
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,14 +8,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class ProductRepositoryTest {
-
     @InjectMocks
     ProductRepository productRepository;
-
     @BeforeEach
     void setUp() {
     }
@@ -27,10 +25,10 @@ class ProductRepositoryTest {
         product.setProductName("Sampo Cap Bambang");
         product.setProductQuantity(100);
         productRepository.create(product);
-
         Iterator<Product> productIterator = productRepository.findAll();
         assertTrue(productIterator.hasNext());
         Product savedProduct = productIterator.next();
+
         assertEquals(product.getProductId(), savedProduct.getProductId());
         assertEquals(product.getProductName(),savedProduct.getProductName());
         assertEquals(product.getProductQuantity(),savedProduct.getProductQuantity());
@@ -47,13 +45,11 @@ class ProductRepositoryTest {
         product1.setProductName("Sampo Cap Bambang");
         product1.setProductQuantity(100);
         productRepository.create(product1);
-
         Product product2 = new Product();
         product2.setProductId("a0f9de46-98b1-437d-a0bf-d0821dde9896");
         product2.setProductName("Sampo Cap Usep");
         product2.setProductQuantity(50);
         productRepository.create(product2);
-
         Iterator<Product> productIterator = productRepository.findAll();
         assertTrue(productIterator.hasNext());
         Product savedProduct = productIterator.next();
@@ -63,4 +59,46 @@ class ProductRepositoryTest {
         assertFalse(productIterator.hasNext());
     }
 
+    @Test
+    void testEdit(){
+
+        // Make new product
+        Product product = new Product();
+        product.setProductId("abcde");
+        product.setProductName("Nanas");
+        product.setProductQuantity(10);
+        productRepository.create(product);
+
+        // Change the name & quantity of product
+        product = productRepository.findById("abcde");
+        product.setProductName("Nanas busuk");
+        product.setProductQuantity(8);
+        productRepository.save(product);
+
+        // Verify
+        product = productRepository.findById("abcde");
+        assertEquals(product.getProductId(), "abcde");
+        assertEquals(product.getProductName(), "Nanas busuk");
+        assertEquals(product.getProductQuantity(), 8);
+    }
+
+    @Test
+    void testDelete(){
+        // Make new product
+        Product product = new Product();
+        product.setProductId("abcde");
+        product.setProductName("Nanas");
+        product.setProductQuantity(10);
+        productRepository.create(product);
+
+        // Delete product
+        productRepository.deleteProductById("abcde");
+
+        // Verify
+        Iterator<Product> productIterator = productRepository.findAll();
+        while (productIterator.hasNext()){
+            assertNotEquals(productIterator.next().getProductId(), "abcde");
+        }
+
+    }
 }
