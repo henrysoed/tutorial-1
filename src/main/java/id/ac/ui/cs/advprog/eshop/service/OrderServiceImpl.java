3d87cbc0bf19a0178f33c27e77323f0a965e996a
Henrysoed <henry.soedibjo@gmail.com>
2024-03-05 17:55:12 +0700

    Revise OrderServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -1,11 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.service;
+
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -13,34 +14,14 @@ public class OrderServiceImpl implements OrderService {
     private OrderRepository orderRepository;
 
     @Override
-    public Order createOrder(Order order) {
-        if (orderRepository.findById(order.getId()) == null) {
-            orderRepository.save(order);
-            return order;
-        }
-        return null;
-    }
+    public Order createOrder(Order order) { return null; }
 
     @Override
-    public Order updateStatus(String orderId, String status) {
-        Order order = orderRepository.findById(orderId);
-        if (order != null) {
-            Order newOrder = new Order(order.getId(), order.getProducts(),
-                    order.getOrderTime(), order.getAuthor(), status);
-            orderRepository.save(newOrder);
-            return newOrder;
-        } else {
-            throw new NoSuchElementException();
-        }
-    }
+    public Order updateStatus(String orderId, String status) { return null; }
 
     @Override
-    public Order findById(String orderId) {
-        return orderRepository.findById(orderId);
-    }
+    public List<Order> findAllByAuthor(String author) { return null; }
 
     @Override
-    public List<Order> findAllByAuthor(String author) {
-        return orderRepository.findAllByAuthor(author);
-    }
+    public Order findById(String orderId) { return null; }
 }
