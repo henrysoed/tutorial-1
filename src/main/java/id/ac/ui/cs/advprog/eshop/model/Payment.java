fe85063533277f953540ce59b93d7acbfb3df0e4
Henrysoed <henry.soedibjo@gmail.com>
2024-03-06 19:41:55 +0700

    [REFACTOR] assign payment data only for valid payment methods

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,8 +1,12 @@
 package id.ac.ui.cs.advprog.eshop.model;
+
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
 import lombok.Getter;
+
 import java.util.Map;
+
 @Getter
 public class Payment {
     String id;
@@ -10,6 +14,7 @@ public class Payment {
     Order order;
     Map<String, String> paymentData;
     String status;
+
     public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
         this.id = id;
         this.method = method;
@@ -17,17 +22,18 @@ public class Payment {
         this.setPaymentData(paymentData);
         this.setStatus(status);
     }
+
     public Payment(String id, String method, Order order, Map<String, String> paymentData) {
         this(id, method, order, paymentData, PaymentStatus.PENDING.getValue());
+
     }
     private void setOrder(Order order) {
-        if (order == null) {
-            throw new IllegalArgumentException("Order cannot be null");
-        } else {
+        if (order != null) {
             this.order = order;
+        } else {
+            throw new IllegalArgumentException("Order cannot be null");
         }
     }
-
     public void setStatus(String status) {
         if (PaymentStatus.contains(status)) {
             this.status = status;
@@ -35,12 +41,11 @@ public class Payment {
             throw new IllegalArgumentException("Invalid payment status");
         }
     }
-
-    protected void setPaymentData(Map<String, String>paymentData) {
+    protected void setPaymentData(Map<String, String> paymentData) {
         if (PaymentMethod.contains(this.method)) {
-            throw new IllegalArgumentException("Unable to assign payment data specific to a method when the payment method is not specified");
+            this.paymentData = paymentData;
         } else {
-            this.paymentData = null;
+            throw new IllegalArgumentException("Unable to assign payment data specific to a method when the payment method is not specified");
         }
     }
 }
